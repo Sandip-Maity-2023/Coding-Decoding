535ad857d61e988a6d81227c48b4af0b618e5c4f
Sandip Maity <145787637+Sandip-Maity-2023@users.noreply.github.com>
2024-01-10 23:25:17 +0530

    Update 011 c program to display student details that entered by the user using structure function.c

--- 011 c program to display student details that entered by the user using structure function.c
+++ 011 c program to display student details that entered by the user using structure function.c
@@ -3,7 +3,7 @@ int main(){
     struct stu_details{
         char name[15];
         int age;
-        long int ph_no;
+        long int ph_no;//
     };
     struct stu_details sd={"SANDIP MAITY",21,9002838296};
     printf("%s\n",sd.name);
